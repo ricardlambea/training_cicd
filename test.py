07988ef4b7104a99f74eef014702d3651335ad69
Ricard Lambea Jané <56436468+ricardlambea@users.noreply.github.com>
2024-06-11 10:41:09 +0200

    Update test.py

--- test.py
+++ test.py
@@ -6,16 +6,20 @@ import transform
 
 
 class TestStringMethods(unittest.TestCase):
+    """Test main.py"""
 
     def test_is_upper(self):
+        """Test main.py"""
         sting = transform.to_upper_case("hello")
         self.assertEqual(sting, "HELLO")
 
     def test_is_lower(self):
+        """Test main.py"""
         sting = transform.to_lower_case("HELLO")
         self.assertEqual(sting, "hello")
 
     def test_is_capitalize(self):
+        """Test main.py"""
         sting = transform.to_capitalize("HELLO")
         self.assertEqual(sting, "Hello")
 
